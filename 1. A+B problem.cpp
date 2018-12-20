b6bdc3600c17103aeeca2f3afad7ea721d7c4d8e
Roc-J <646481338@qq.com>
2018-12-20 22:02:15 +0800

    A+b

---
+++ 1. A+B problem.cpp
@@ -0,0 +1,18 @@
+/*
+输入两个整数，输出这两个数字的和
+input: a, b 
+output: a+b 
+*/
+
+class Solution {
+public:
+    /**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: The sum of a and b 
+     */
+    int aplusb(int a, int b) {
+        // write your code here
+        return a+b;
+    }
+};
